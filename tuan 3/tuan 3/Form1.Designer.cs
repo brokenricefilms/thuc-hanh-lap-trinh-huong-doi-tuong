4f65be9ae821b22e36e9fae177ad5cb28e086925
Thuan Pham <phamtanthuan2311@gmail.com>
2021-11-05 00:56:10 +0700

    [👌Auto commit] ...

--- tuan 3/tuan 3/Form1.Designer.cs
+++ tuan 3/tuan 3/Form1.Designer.cs
@@ -1,4 +1,7 @@
 ﻿
+using System;
+using System.Windows.Forms;
+
 namespace tuan_3
 {
     partial class frmNSX
@@ -30,16 +33,13 @@ namespace tuan_3
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 =
-                new System.Windows.Forms.DataGridViewCellStyle();
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle5 =
-                new System.Windows.Forms.DataGridViewCellStyle();
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle6 =
-                new System.Windows.Forms.DataGridViewCellStyle();
-            this.button1 = new System.Windows.Forms.Button();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.btnThem = new System.Windows.Forms.Button();
             this.btnXoa = new System.Windows.Forms.Button();
             this.btnSua = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
+            this.btnThoat = new System.Windows.Forms.Button();
             this.txtMaNSX = new System.Windows.Forms.TextBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.txtSoDT = new System.Windows.Forms.TextBox();
@@ -56,19 +56,19 @@ namespace tuan_3
             this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             ((System.ComponentModel.ISupportInitialize)(this.dgvNSX)).BeginInit();
             this.SuspendLayout();
-            //
-            // button1
-            //
-            this.button1.Location = new System.Drawing.Point(365, 44);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(100, 30);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Thêm";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            //
+            // 
+            // btnThem
+            // 
+            this.btnThem.Location = new System.Drawing.Point(365, 44);
+            this.btnThem.Name = "btnThem";
+            this.btnThem.Size = new System.Drawing.Size(100, 30);
+            this.btnThem.TabIndex = 0;
+            this.btnThem.Text = "Thêm";
+            this.btnThem.UseVisualStyleBackColor = true;
+            this.btnThem.Click += new System.EventHandler(this.button1_Click);
+            // 
             // btnXoa
-            //
+            // 
             this.btnXoa.Location = new System.Drawing.Point(365, 80);
             this.btnXoa.Name = "btnXoa";
             this.btnXoa.Size = new System.Drawing.Size(100, 30);
@@ -76,9 +76,9 @@ namespace tuan_3
             this.btnXoa.Text = "Xóa ";
             this.btnXoa.UseVisualStyleBackColor = true;
             this.btnXoa.Click += new System.EventHandler(this.button2_Click);
-            //
+            // 
             // btnSua
-            //
+            // 
             this.btnSua.Location = new System.Drawing.Point(365, 116);
             this.btnSua.Name = "btnSua";
             this.btnSua.Size = new System.Drawing.Size(100, 30);
@@ -86,47 +86,47 @@ namespace tuan_3
             this.btnSua.Text = "Sửa";
             this.btnSua.UseVisualStyleBackColor = true;
             this.btnSua.Click += new System.EventHandler(this.button3_Click);
-            //
-            // button4
-            //
-            this.button4.Location = new System.Drawing.Point(365, 152);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(100, 30);
-            this.button4.TabIndex = 3;
-            this.button4.Text = "Thoát";
-            this.button4.UseVisualStyleBackColor = true;
-            this.button4.Click += new System.EventHandler(this.button4_Click);
-            //
+            // 
+            // btnThoat
+            // 
+            this.btnThoat.Location = new System.Drawing.Point(365, 152);
+            this.btnThoat.Name = "btnThoat";
+            this.btnThoat.Size = new System.Drawing.Size(100, 30);
+            this.btnThoat.TabIndex = 3;
+            this.btnThoat.Text = "Thoát";
+            this.btnThoat.UseVisualStyleBackColor = true;
+            this.btnThoat.Click += new System.EventHandler(this.button4_Click);
+            // 
             // txtMaNSX
-            //
+            // 
             this.txtMaNSX.Location = new System.Drawing.Point(140, 44);
             this.txtMaNSX.Name = "txtMaNSX";
             this.txtMaNSX.Size = new System.Drawing.Size(147, 20);
             this.txtMaNSX.TabIndex = 4;
-            //
+            // 
             // textBox2
-            //
+            // 
             this.textBox2.Location = new System.Drawing.Point(140, 152);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(147, 20);
             this.textBox2.TabIndex = 5;
-            //
+            // 
             // txtSoDT
-            //
+            // 
             this.txtSoDT.Location = new System.Drawing.Point(140, 116);
             this.txtSoDT.Name = "txtSoDT";
             this.txtSoDT.Size = new System.Drawing.Size(147, 20);
             this.txtSoDT.TabIndex = 6;
-            //
+            // 
             // txtTenNSX
-            //
+            // 
             this.txtTenNSX.Location = new System.Drawing.Point(140, 80);
             this.txtTenNSX.Name = "txtTenNSX";
             this.txtTenNSX.Size = new System.Drawing.Size(147, 20);
             this.txtTenNSX.TabIndex = 7;
-            //
+            // 
             // label1
-            //
+            // 
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(13, 44);
             this.label1.Name = "label1";
@@ -134,9 +134,9 @@ namespace tuan_3
             this.label1.Size = new System.Drawing.Size(0, 13);
             this.label1.TabIndex = 8;
             this.label1.Click += new System.EventHandler(this.label1_Click);
-            //
+            // 
             // label2
-            //
+            // 
             this.label2.AutoSize = true;
             this.label2.Location = new System.Drawing.Point(20, 50);
             this.label2.Name = "label2";
@@ -144,105 +144,95 @@ namespace tuan_3
             this.label2.TabIndex = 9;
             this.label2.Text = "Mã nhà sản xuất";
             this.label2.Click += new System.EventHandler(this.label2_Click);
-            //
+            // 
             // label3
-            //
+            // 
             this.label3.AutoSize = true;
             this.label3.Location = new System.Drawing.Point(20, 87);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(90, 13);
             this.label3.TabIndex = 10;
             this.label3.Text = "Tên nhà sản xuất";
-            //
+            // 
             // label4
-            //
+            // 
             this.label4.AutoSize = true;
             this.label4.Location = new System.Drawing.Point(23, 123);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(70, 13);
             this.label4.TabIndex = 11;
             this.label4.Text = "Số điện thoại";
-            //
+            // 
             // label5
-            //
+            // 
             this.label5.AutoSize = true;
             this.label5.Location = new System.Drawing.Point(23, 159);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(40, 13);
             this.label5.TabIndex = 12;
             this.label5.Text = "Địa chỉ";
-            //
+            // 
             // dgvNSX
-            //
-            dataGridViewCellStyle4.Alignment =
-                System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
-            dataGridViewCellStyle4.BackColor = System.Drawing.SystemColors.Control;
-            dataGridViewCellStyle4.Font =
-                new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular,
-                                        System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            dataGridViewCellStyle4.ForeColor = System.Drawing.SystemColors.WindowText;
-            dataGridViewCellStyle4.SelectionBackColor = System.Drawing.SystemColors.Highlight;
-            dataGridViewCellStyle4.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
-            dataGridViewCellStyle4.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
-            this.dgvNSX.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle4;
-            this.dgvNSX.ColumnHeadersHeightSizeMode =
-                System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            // 
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dgvNSX.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            this.dgvNSX.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dgvNSX.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
-        this.Column1, this.Column2, this.Column3, this.Column4
-      });
-            dataGridViewCellStyle5.Alignment =
-                System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
-            dataGridViewCellStyle5.BackColor = System.Drawing.SystemColors.Window;
-            dataGridViewCellStyle5.Font =
-                new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular,
-                                        System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            dataGridViewCellStyle5.ForeColor = System.Drawing.SystemColors.ControlText;
-            dataGridViewCellStyle5.SelectionBackColor = System.Drawing.SystemColors.Highlight;
-            dataGridViewCellStyle5.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
-            dataGridViewCellStyle5.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
-            this.dgvNSX.DefaultCellStyle = dataGridViewCellStyle5;
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4});
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dgvNSX.DefaultCellStyle = dataGridViewCellStyle2;
             this.dgvNSX.Location = new System.Drawing.Point(26, 197);
             this.dgvNSX.Name = "dgvNSX";
-            dataGridViewCellStyle6.Alignment =
-                System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
-            dataGridViewCellStyle6.BackColor = System.Drawing.SystemColors.Control;
-            dataGridViewCellStyle6.Font =
-                new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular,
-                                        System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            dataGridViewCellStyle6.ForeColor = System.Drawing.SystemColors.WindowText;
-            dataGridViewCellStyle6.SelectionBackColor = System.Drawing.SystemColors.Highlight;
-            dataGridViewCellStyle6.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
-            dataGridViewCellStyle6.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
-            this.dgvNSX.RowHeadersDefaultCellStyle = dataGridViewCellStyle6;
+            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle3.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle3.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle3.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dgvNSX.RowHeadersDefaultCellStyle = dataGridViewCellStyle3;
             this.dgvNSX.Size = new System.Drawing.Size(447, 218);
             this.dgvNSX.TabIndex = 13;
-            this.dgvNSX.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(
-                this.dataGridView1_CellContentClick);
-            this.dgvNSX.RowEnter += new System.Windows.Forms.DataGridViewCellEventHandler(
-                this.dataGridView1_CellContentClick);
-            //
+            this.dgvNSX.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            this.dgvNSX.RowEnter += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
             // Column1
-            //
+            // 
             this.Column1.HeaderText = "Mã nhà sản xuất";
             this.Column1.Name = "Column1";
-            //
+            // 
             // Column2
-            //
+            // 
             this.Column2.HeaderText = "Tên nhà sản xuất";
             this.Column2.Name = "Column2";
-            //
+            // 
             // Column3
-            //
+            // 
             this.Column3.HeaderText = "Số điện thoại";
             this.Column3.Name = "Column3";
-            //
+            // 
             // Column4
-            //
+            // 
             this.Column4.HeaderText = "Địa chỉ";
             this.Column4.Name = "Column4";
-            //
-            // Form1
-            //
+            // 
+            // frmNSX
+            // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(497, 437);
@@ -256,24 +246,65 @@ namespace tuan_3
             this.Controls.Add(this.txtSoDT);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.txtMaNSX);
-            this.Controls.Add(this.button4);
+            this.Controls.Add(this.btnThoat);
             this.Controls.Add(this.btnSua);
             this.Controls.Add(this.btnXoa);
-            this.Controls.Add(this.button1);
-            this.Name = "Form1";
+            this.Controls.Add(this.btnThem);
+            this.Name = "frmNSX";
             this.Text = "Quản lý nhà sản xuất";
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dgvNSX)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
         }
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnThem;
         private System.Windows.Forms.Button btnXoa;
         private System.Windows.Forms.Button btnSua;
-        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button btnThoat;
         private System.Windows.Forms.TextBox txtMaNSX;
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.TextBox txtSoDT;
--- tuan 3/tuan 3/Form1.cs
+++ tuan 3/tuan 3/Form1.cs
@@ -8,123 +8,159 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace tuan_3 {
+namespace tuan_3
+{
 
-  public partial class frmNSX : Form {
-    private Dictionary<string, CNhaSanXuat> dsNSX;
-    private void hienDSNhaSanXuat() {
-      dgvNSX.DataSource = dsNSX.Values.ToList();
-    }
-    private CNhaSanXuat timNSX(string ma) {
-      try {
-        return dsNSX[ma];
-      } catch {
-        return null;
-      }
-    }
-    public frmNSX() {
-      InitializeComponent();
-    }
-    private void frmNSX_Load(object sender, EventArgs e) {
-      dsNSX = new Dictionary<string, CNhaSanXuat>();
-    }
-    private void btnThem_Click(object sender, EventArgs e) {
-      CNhaSanXuat n = new CNhaSanXuat();
-      n.MaNSX = txtMaNSX.Text;
-      n.TenNSX = txtTenNSX.Text;
-      n.SoDT = txtSoDT.Text;
-      n.DiaChi = txtDiaChi.Text;
-      if (timNSX(n.MaNSX) == null) {
-        dsNSX.Add(n.MaNSX, n);
-        hienDSNhaSanXuat();
-      } else
-MessageBox.Show("Mã nhà sản xuất " + n.MaNSX + " đã tồn tại.
-Không thêm được.");
-    }
-    private void dgvNSX_RowEnter(object sender, DataGridViewCellEventArgs e) {
-      txtMaNSX.Text = dgvNSX.Rows[e.RowIndex].Cells[0].Value.ToString();
-      txtTenNSX.Text = dgvNSX.Rows[e.RowIndex].Cells[1].Value.ToString();
-      txtSoDT.Text = dgvNSX.Rows[e.RowIndex].Cells[2].Value.ToString();
-      txtDiaChi.Text = dgvNSX.Rows[e.RowIndex].Cells[3].Value.ToString();
-    }
-    private void btnThoat_Click(object sender, EventArgs e) {
-      Close();
-    }
-  }
-
-  public class CNhaSanXuat {
-    private string m_mansx, m_tennsx, m_sodt, m_diachi;
-    public string MaNSX {
-      get { return m_mansx; }
-      set { m_mansx = value; }
-    }
-    public string TenNSX {
-      get { return m_tennsx; }
-      set { m_tennsx = value; }
-    }
-    public string SoDT {
-      get { return m_sodt; }
-      set { m_sodt = value; }
-    }
-    public string DiaChi {
-      get { return m_diachi; }
-      set { m_diachi = value; }
-    }
-    public CNhaSanXuat() {
-      m_mansx = "";
-      m_tennsx = "";
-      m_sodt = "";
-      m_diachi = "";
-    }
-    public CNhaSanXuat(string ma, string ten, string sodt, string dc) {
-      m_mansx = ma;
-      m_tennsx = ten;
-      m_sodt = sodt;
-      m_diachi = dc;
-    }
-  }
-  public partial class Form1 : Form {
-    public Form1() {
-      InitializeComponent();
-    }
-
-    private void Form1_Load(object sender, EventArgs e) {}
-
-    private void button2_Click(object sender, EventArgs e) {
-      CNhaSanXuat n = timNSX(txtMaNSX.Text);
-      if (n == null) {
-        MessageBox.Show("Khong co nsx nay");
-      } else {
-        if (MessageBox.Show("Ban muon xoa NSX nay?", "Thong bao", MessageBoxButtons.OKCancel,
-                            MessageBoxButtons.Question) == DialogResult.OK) {
-          dsNSX.Remove(n.MaNSX);
-          hienDSNhaSanXuat();
+    public partial class frmNSX : Form
+    {
+        private Dictionary<string, CNhaSanXuat> dsNSX;
+        private void hienDSNhaSanXuat()
+        {
+            dgvNSX.DataSource = dsNSX.Values.ToList();
         }
-      }
+        private CNhaSanXuat timNSX(string ma)
+        {
+            try
+            {
+                return dsNSX[ma];
+            }
+            catch
+            {
+                return null;
+            }
+        }
+        public frmNSX()
+        {
+            InitializeComponent();
+        }
+        private void frmNSX_Load(object sender, EventArgs e)
+        {
+            dsNSX = new Dictionary<string, CNhaSanXuat>();
+        }
+        private void btnThem_Click(object sender, EventArgs e)
+        {
+            CNhaSanXuat n = new CNhaSanXuat();
+            n.MaNSX = txtMaNSX.Text;
+            n.TenNSX = txtTenNSX.Text;
+            n.SoDT = txtSoDT.Text;
+            n.DiaChi = txtDiaChi.Text;
+            if (timNSX(n.MaNSX) == null)
+            {
+                dsNSX.Add(n.MaNSX, n);
+                hienDSNhaSanXuat();
+            }
+            else
+                MessageBox.Show("Mã nhà sản xuất " + n.MaNSX + " đã tồn tại. Không thêm được.");
+        }
+        private void dgvNSX_RowEnter(object sender, DataGridViewCellEventArgs e)
+        {
+            txtMaNSX.Text = dgvNSX.Rows[e.RowIndex].Cells[0].Value.ToString();
+            txtTenNSX.Text = dgvNSX.Rows[e.RowIndex].Cells[1].Value.ToString();
+            txtSoDT.Text = dgvNSX.Rows[e.RowIndex].Cells[2].Value.ToString();
+            txtDiaChi.Text = dgvNSX.Rows[e.RowIndex].Cells[3].Value.ToString();
+        }
+        private void btnThoat_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
     }
 
-    private void button1_Click(object sender, EventArgs e) {}
-
-    private void button4_Click(object sender, EventArgs e) {}
-
-    private void button3_Click(object sender, EventArgs e) {
-      CNhaSanXuat n = timNSX(txtMaNSX.Text);
-      if (n == null) {
-        MessageBox.Show("Khong tim thay");
-      } else {
-        n.TenNSX = txtTenNSX.Text;
-        n.SoDT = txtSoDT.Text;
-        n.DiaChi = txtDiaChi.text;
-        hienDSNhaSanXuat();
-      }
+    public class CNhaSanXuat
+    {
+        private string m_mansx, m_tennsx, m_sodt, m_diachi;
+        public string MaNSX
+        {
+            get { return m_mansx; }
+            set { m_mansx = value; }
+        }
+        public string TenNSX
+        {
+            get { return m_tennsx; }
+            set { m_tennsx = value; }
+        }
+        public string SoDT
+        {
+            get { return m_sodt; }
+            set { m_sodt = value; }
+        }
+        public string DiaChi
+        {
+            get { return m_diachi; }
+            set { m_diachi = value; }
+        }
+        public CNhaSanXuat()
+        {
+            m_mansx = "";
+            m_tennsx = "";
+            m_sodt = "";
+            m_diachi = "";
+        }
+        public CNhaSanXuat(string ma, string ten, string sodt, string dc)
+        {
+            m_mansx = ma;
+            m_tennsx = ten;
+            m_sodt = sodt;
+            m_diachi = dc;
+        }
     }
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
 
-    private void richTextBox1_TextChanged(object sender, EventArgs e) {}
+        private void InitializeComponent()
+        {
+            throw new NotImplementedException();
+        }
 
-    private void label1_Click(object sender, EventArgs e) {}
+        private void Form1_Load(object sender, EventArgs e) { }
 
-    private void label2_Click(object sender, EventArgs e) {}
+        private void button2_Click(object sender, EventArgs e)
+        {
+            CNhaSanXuat n = timNSX(txtMaNSX.Text);
+            if (n == null)
+            {
+                MessageBox.Show("Khong co nsx nay");
+            }
+            else
+            {
+                if (MessageBox.Show("Ban muon xoa NSX nay?", "Thong bao", MessageBoxButtons.OKCancel,
+                                    MessageBoxButtons.Question) == DialogResult.OK)
+                {
+                    dsNSX.Remove(n.MaNSX);
+                    hienDSNhaSanXuat();
+                }
+            }
+        }
 
-    private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e) {}
-  }
+        private void button1_Click(object sender, EventArgs e) { }
+
+        private void button4_Click(object sender, EventArgs e) { }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            CNhaSanXuat n = timNSX(txtMaNSX.Text);
+            if (n == null)
+            {
+                MessageBox.Show("Khong tim thay");
+            }
+            else
+            {
+                n.TenNSX = txtTenNSX.Text;
+                n.SoDT = txtSoDT.Text;
+                n.DiaChi = txtDiaChi.text;
+                hienDSNhaSanXuat();
+            }
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e) { }
+
+        private void label1_Click(object sender, EventArgs e) { }
+
+        private void label2_Click(object sender, EventArgs e) { }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e) { }
+    }
 }
