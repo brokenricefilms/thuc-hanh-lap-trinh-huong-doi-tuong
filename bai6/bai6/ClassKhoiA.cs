5ad560185b5427c9051b208701c28ced90dbe6eb
Thuan Pham <phamtanthuan2311@gmail.com>
2021-11-25 11:56:22 +0700

    [👌Auto commit] Revert "fuckup".

---
+++ bai6/bai6/ClassKhoiA.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bai6
+{
+    public class ClassKhoiA : ClassThiSinh
+    {
+        protected double _diemToan;
+        protected double _diemLy;
+        protected double _diemHoa;
+        public double DiemToan
+        {
+            get { return _diemToan; }
+            set { _diemToan = value; }
+        }
+        public double DiemLy
+        {
+            get { return _diemLy; }
+            set { _diemLy = value; }
+        }
+        public double DiemHoa
+        {
+            get { return _diemHoa; }
+            set { _diemHoa = value; }
+        }
+        public ClassKhoiA(){
+            _diemToan = 0;
+            _diemHoa = 0;
+            _diemLy = 0;
+        }
+        public ClassKhoiA(string ma, string ht, double toan, double ly, double hoa){
+            _maTs = ma;
+            _hoTen = ht;
+            _diemToan = toan;
+            _diemLy = ly;
+            _diemHoa = hoa;
+        }
+        public override double TongDiem()
+        {
+            return _diemToan + _diemLy + _diemHoa;
+        }
+        public override KieuKetQua KetQua()
+        {
+            if (_diemHoa + _diemLy + _diemToan >= 18.5)
+            {
+            }
+        }
+        public override bool laKhoiA()
+        {
+            return true;
+        }
+        public override bool laKhoiNangKhieu()
+        {
+            return false;
+        }
+    }
+}
---
+++ bai6/bai6/ClassThiSinh.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bai6
+{
+    public enum KieuKetQua { Dat, KhongDat }
+    public class ClassThiSinh
+    {
+        protected string _maTs;
+        protected string _hoTen;
+        public string maTs
+        {
+            get { return _maTs; }
+            set { _maTs = value; }
+        }
+        public string Hoten
+        {
+            get { return _hoTen; }
+            set { _hoTen = value; }
+        }
+        public ClassThiSinh()
+        {
+            _maTs = "";
+            _hoTen = "";
+        }
+        public ClassThiSinh(string ma, string ht)
+        {
+            _maTs = ma;
+            _hoTen = ht;
+        }
+        public virtual double TongDiem()
+        {
+            return 0;
+        }
+        public virtual KieuKetQua KetQua()
+        {
+            return KieuKetQua.KhongDat;
+        }
+    }
+}
