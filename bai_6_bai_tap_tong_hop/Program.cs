a62fe9293147c2c15c19e67d3f6f6e772a206d11
Thuan Pham <phamtanthuan2311@gmail.com>
2021-12-02 01:00:27 +0700

    [👌Auto commit] Fixed unnecessary bug.

--- bai_6_bai_tap_tong_hop/Program.cs
+++ bai_6_bai_tap_tong_hop/Program.cs
@@ -67,7 +67,7 @@ namespace bai_6_bai_tap_tong_hop {
       return max;
     }
 
-    static List<ClassThiSinh> getDSThiSinhDat(List<ClassThiSinh> danhSach) {
+    static List<ClassThiSinh> getDanhSachThiSinhDat(List<ClassThiSinh> danhSach) {
       List<ClassThiSinh> tmp = new List<ClassThiSinh>();
       foreach (ClassThiSinh i in danhSach) {
         if (i.KetQua() == KieuKetQua.Dat) {
@@ -77,32 +77,101 @@ namespace bai_6_bai_tap_tong_hop {
       return tmp;
     }
 
+    static List<ClassThiSinh> getDanhSachThiSinhKhongDat(List<ClassThiSinh> danhSach) {
+      List<ClassThiSinh> tmp = new List<ClassThiSinh>();
+      foreach (ClassThiSinh i in danhSach) {
+        if (i.KetQua() == KieuKetQua.KhongDat) {
+          tmp.Add(i);
+        }
+      }
+      return tmp;
+    }
+
+    static List<ClassKhoiNangKhieu> getDanhSachThiSinhNangKhieuKhongDat(
+        List<ClassThiSinh> danhSach) {
+      List<ClassKhoiNangKhieu> tmp = new List<ClassKhoiNangKhieu>();
+      foreach (ClassKhoiNangKhieu i in danhSach) {
+        if (i.KetQua() == KieuKetQua.KhongDat) {
+          tmp.Add(i);
+        }
+      }
+      return tmp;
+    }
+
+    // static List<ClassKhoiA> getDanhSachThiSinhKhoiADiemLiet(List<ClassThiSinh> danhSach) {
+    // List<ClassKhoiA> tmp = new List<ClassKhoiA>();
+    // foreach (ClassKhoiA i in danhSach) {
+    // if (i.DiemToan() <= 1) {
+    // tmp.Add(i);
+    // }
+    // }
+    // return tmp;
+    // }
+
     static void Main(string[] args) {
       List<ClassThiSinh> danhSachThiSinh = new List<ClassThiSinh>();
       List<ClassThiSinh> n = new List<ClassThiSinh>();
 
-      danhSachThiSinh.Add(new ClassKhoiA("dh51", "Nicholas", 5, 5, 5));
-      danhSachThiSinh.Add(new ClassKhoiA("dh52", "Nick", 9, 9, 9.5));
-      danhSachThiSinh.Add(new ClassKhoiA("dh53", "Nico", 7, 7, 7));
-      danhSachThiSinh.Add(new ClassKhoiNangKhieu("dh54", "Thuận good boy", 9.5, 9.5));
+      // data gia phả nồi giống Thuận Bá Vương
+      danhSachThiSinh.Add(new ClassKhoiA("dh51", "Thuận ngiu ngốc", 3, 3, 3));
+      danhSachThiSinh.Add(new ClassKhoiA("dh52", "Thuận cái bang", 1, 1.5, 2));
+      danhSachThiSinh.Add(new ClassKhoiA("dh53", "Thuận tính nam", 7, 7, 7.5));
+      danhSachThiSinh.Add(new ClassKhoiNangKhieu("dh54", "Thuận tham ngủ", 10, 0));
       danhSachThiSinh.Add(new ClassKhoiNangKhieu("dh55", "Thuận đẹp trai", 10, 10));
       danhSachThiSinh.Add(new ClassKhoiNangKhieu("dh56", "Thuận siêu cute", 9.5, 10));
 
       Console.WriteLine("\nDanh sách thí sinh khối năng khiếu:");
-      Console.WriteLine("Số thí sinh thuộc khối năng khiếu = " +
-                        demSoThiSinhKhoiNangkhieu(danhSachThiSinh));
+      Console.WriteLine("-----------------------------");
+      Console.WriteLine("Mã thí sinh" + " | " + "Họ và tên");
+      Console.WriteLine("-----------------------------");
       foreach (ClassThiSinh thiSinh in getDanhSachThiSinhKhoiNangKhieu(danhSachThiSinh)) {
-        Console.WriteLine(thiSinh.MaThiSinh + " - " + thiSinh.Hoten);
+        Console.WriteLine(thiSinh.MaThiSinh + "        | " + thiSinh.Hoten);
       }
-      Console.WriteLine("Điểm lớn nhất là " + getDiemLonNhatKhoiNangKhieu(danhSachThiSinh));
+      Console.WriteLine("→ Số thí sinh thuộc khối năng khiếu = " +
+                        demSoThiSinhKhoiNangkhieu(danhSachThiSinh));
+      Console.WriteLine("→ Điểm lớn nhất là " + getDiemLonNhatKhoiNangKhieu(danhSachThiSinh));
 
-      Console.WriteLine("\nDanh sách thí sinh khối A:");
-      Console.WriteLine("Số thí sinh thuộc khối A = " + demSoThiSinhKhoiA(danhSachThiSinh));
+      Console.WriteLine("\nDanh sách thí sinh khối A");
+      Console.WriteLine("-----------------------------");
+      Console.WriteLine("Mã thí sinh" + " | " + "Họ và tên");
+      Console.WriteLine("-----------------------------");
       foreach (ClassThiSinh thiSinh in getDanhSachThiSinhKhoiA(danhSachThiSinh)) {
-        Console.WriteLine(thiSinh.MaThiSinh + " - " + thiSinh.Hoten);
+        Console.WriteLine(thiSinh.MaThiSinh + "        | " + thiSinh.Hoten);
       }
-      Console.WriteLine("Điểm lớn nhất là " + getDiemLonNhatKhoiA(danhSachThiSinh));
-      Console.ReadLine();
+      Console.WriteLine("→ Số thí sinh thuộc khối A = " + demSoThiSinhKhoiA(danhSachThiSinh));
+      Console.WriteLine("→ Điểm lớn nhất là " + getDiemLonNhatKhoiA(danhSachThiSinh));
+
+      Console.WriteLine("\nDanh sách thí sinh đạt");
+      Console.WriteLine("-----------------------------");
+      Console.WriteLine("Mã thí sinh" + " | " + "Họ và tên");
+      Console.WriteLine("-----------------------------");
+      foreach (ClassThiSinh thiSinh in getDanhSachThiSinhDat(danhSachThiSinh)) {
+        Console.WriteLine(thiSinh.MaThiSinh + "        | " + thiSinh.Hoten);
+      }
+
+      Console.WriteLine("\nDanh sách thí sinh không đạt");
+      Console.WriteLine("-----------------------------");
+      Console.WriteLine("Mã thí sinh" + " | " + "Họ và tên");
+      Console.WriteLine("-----------------------------");
+      foreach (ClassThiSinh thiSinh in getDanhSachThiSinhKhongDat(danhSachThiSinh)) {
+        Console.WriteLine(thiSinh.MaThiSinh + "        | " + thiSinh.Hoten);
+      }
+
+      Console.WriteLine("\nDanh sách thí sinh năng khiếu không đạt");
+      Console.WriteLine("-----------------------------");
+      Console.WriteLine("Mã thí sinh" + " | " + "Họ và tên");
+      Console.WriteLine("-----------------------------");
+      foreach (ClassKhoiNangKhieu thiSinh in getDanhSachThiSinhNangKhieuKhongDat(danhSachThiSinh)) {
+        Console.WriteLine(thiSinh.MaThiSinh + "        | " + thiSinh.Hoten);
+      }
+
+      Console.WriteLine("\nDanh sách thí sinh Khối A bị điểm liệt");
+      Console.WriteLine("-----------------------------");
+      Console.WriteLine("Mã thí sinh" + " | " + "Họ và tên");
+      Console.WriteLine("-----------------------------");
+      // foreach (ClassThiSinh thiSinh in getDanhSachThiSinhKhoiADiemLiet(danhSachThiSinh)) {
+      // Console.WriteLine(thiSinh.MaThiSinh + "        | " + thiSinh.Hoten);
+      // }
     }
   }
 }
