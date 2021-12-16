02e5513326cf99d837fc175757c096abed291e2e
Thuan Pham <phamtanthuan2311@gmail.com>
2021-12-16 09:18:17 +0700

    [👌Auto commit] eppic fail Jon

---
+++ bai8-9_thuc_hanh_2/bai8-9_thuc_hanh_2/CXuLyHangHoa.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace thuc_hanh_2
+{
+    class CXuLyHangHoa
+    {
+        private List<CHangHoa> ds;
+        public CXuLyHangHoa()
+        {
+            CTruyCapDuLieu tc = CTruyCapDuLieu.khoitao();
+            ds = tc.getDSHangHoa();
+        }
+        public List<CHangHoa> getDSHangHoa()
+        {
+            return ds;
+        }
+        public CHangHoa tim(string ma)
+        {
+            foreach (CHangHoa i in ds)
+                if (i.MaHang == ma)
+                    return i;
+            return null;
+        }
+        public void them(CHangHoa a)
+        {
+            if (tim(a.MaHang) == null)
+                ds.Add(a);
+            else
+                MessageBox.Show("Mã hàng hóa đã tồn tại trong danh sách hàng hóa. Không thêm được.");
+        }
+        public void xoa(string mahang)
+        {
+            CHangHoa h = tim(mahang);
+            if (h != null)
+                ds.Remove(h);
+            else
+                MessageBox.Show("Mã hàng hóa không tồn tại trong danh sách hàng hóa. Không xóa được.");
+        }
+        public void sua(CHangHoa a)
+        {
+            CHangHoa h = tim(a.MaHang);
+            if (h != null)
+            {
+                h.TenHang = a.TenHang;
+                h.DonGia = a.DonGia;
+                h.DVT = a.DVT;
+            }                
+            else
+                MessageBox.Show("Mã hàng hóa không tồn tại trong danh sách hàng hóa. Không sửa được.");
+        }
+
+    }
+}
